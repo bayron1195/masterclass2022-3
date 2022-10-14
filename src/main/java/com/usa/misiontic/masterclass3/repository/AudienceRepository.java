202bc89a66e65a178f0cc75923f15ee272d28cac
andres.ruiz <bayron.ruiz.mt@usa.edu.co>
2022-10-13 19:58:27 -0500

    cambios
    prueba

---
+++ src/main/java/com/usa/misiontic/masterclass3/repository/AudienceRepository.java
@@ -0,0 +1,24 @@
+package com.usa.misiontic.masterclass3.repository;
+
+import com.usa.misiontic.masterclass3.entities.Audience;
+import com.usa.misiontic.masterclass3.repository.crudRepository.AudienceCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AudienceRepository {
+    @Autowired
+    private AudienceRepository audienceCrudRepository;
+
+    public List<Audience> getAll(){return (List<Audience>) audienceCrudRepository.findAll();}
+
+    public Optional<Audience> getAudience(int id){return audienceCrudRepository.findById(id);}
+    public Audience save(Audience c){return audienceCrudRepository.save(c);}
+    public void delete(Audience c){
+        audienceCrudRepository.delete(c);
+    }
+
+}
--- src/main/java/com/usa/misiontic/masterclass3/service/AudienceService.java
+++ src/main/java/com/usa/misiontic/masterclass3/service/AudienceService.java
@@ -1,6 +1,7 @@
 package com.usa.misiontic.masterclass3.service;
 
 import com.usa.misiontic.masterclass3.entities.Audience;
+import com.usa.misiontic.masterclass3.repository.AudienceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
